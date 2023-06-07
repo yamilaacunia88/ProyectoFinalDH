7d1b6a433d1a0f506dda543bd97f6c3d2e9da677
yami <yamila.acunia@gmail.com>
2023-06-07 20:32:18 -0300

    agregando proyecto

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+let fs = require('fs');
+//implemento rutas
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log ('servidor corriendo en puerto $(PORT)'));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/views/login.html", (req,res) => {
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/views/register.html", (req,res) => {
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/views/register.html", (req,res) => {
+  res.sendFile(__dirname + "/views/dashboard.html");
+});
